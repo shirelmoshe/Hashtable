eb4b289a40a7b2abdcc2fd61cb710409e1c3d26e
shirelmoshe <111507868+shirelmoshe@users.noreply.github.com>
2022-12-02 11:20:55 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            manager.INSTANCE.LoadTable();
+        }
+    }
+}
